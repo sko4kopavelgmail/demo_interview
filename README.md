c8c00384b7a025d198396086132cb372ff3b2543
Skochko Pavel <sko4ko.pavel@gmail.com>
2022-03-30 23:18:10 +0200

    collections task corrected

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 Here we have some tasks connected to collections. Please check the CollectionService.
 
 Tasks:
-1. Write a method which will get a collection as an input information and return map which will 
+1. Write a method which gets a collection as an input and returns map 
+   where the key as the element of the collection, and the value is the number of occurrences of it.
+   
+2. Write a test for the method
--- src/test/java/com/example/demo/intervew/service/CollectionServiceTest.java
+++ src/test/java/com/example/demo/intervew/service/CollectionServiceTest.java
@@ -1,14 +1,28 @@
 package com.example.demo.intervew.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class CollectionServiceTest {
+
+    @Autowired
+    private CollectionService collectionService;
 
     @Test
     void countValues() {
+        Map<Integer, Integer> map = collectionService.countValues(null);
+        assertNull(map);
     }
 }
